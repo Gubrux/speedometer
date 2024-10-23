1e26238cc4941df253b2809d917eb8995ef54c19
gabriel <gabrielcentur@gmail.com>
2024-10-23 09:28:38 -0300

    first and maybe the last commit 🥹

---
+++ src/components/GaugeChartSpeedmeter.vue
@@ -0,0 +1,21 @@
+<template>
+    <VueSpeedometer
+        :value="currentValue"
+        :min-value="0"
+        :max-value="1000"
+        :segments="10"
+        :needle-color="'red'"
+        :start-color="'green'"
+        :end-color="'red'"
+        :ring-width="30"
+        :text-color="'black'"
+        :label="'Custom Speed'"
+    />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import VueSpeedometer from 'vue-speedometer';
+
+const currentValue = ref(500);
+</script>
